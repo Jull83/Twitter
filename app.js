07f681974d233c68fe57f624c5289b1a112e3be6
Jull83 <jull.83@hotmail.com>
2017-12-12 15:30:32 -0600

    Subiendo Twitter

---
+++ app.js
@@ -0,0 +1,8 @@
+var btnEnviar = document.getElementById("btn-tweet");
+btnEnviar.addEventListener("click", agregarTweet);
+var textArea = document.getElementById("mensaje");
+
+function agregarTweet() {
+  
+
+}
